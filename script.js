8778f24321bad1962e3a07053c2eac36443c5555
voltarocket <voltarocket@gmail.com>
2024-12-04 23:26:18 +0300

    commit

--- script.js
+++ script.js
@@ -8,17 +8,20 @@ fetch('modal.html')
     const closeModalButton = document.getElementById("closeModal");
 
     openModalButton.addEventListener("click", () => {
-      modal.style.display = "flex";
-    });
+        modal.style.display = "flex";
+        document.body.classList.add("no-scroll");
+      });
 
     closeModalButton.addEventListener("click", () => {
-      modal.style.display = "none";
-    });
-
-    window.addEventListener("click", (event) => {
-      if (event.target === modal) {
         modal.style.display = "none";
-      }
-    });
-  })
+        document.body.classList.remove("no-scroll"); 
+      });
+
+      window.addEventListener("click", (event) => {
+        if (event.target === modal) {
+          modal.style.display = "none";
+          document.body.classList.remove("no-scroll"); 
+        }
+      });
+    })
   .catch(error => console.error('Ошибка загрузки модального окна:', error));
